af26f090155803f490ed749084ed9a5fc6f3a673
Mohamedali2074 <mohammedali.w74@gmail.com>
2023-02-27 04:39:12 +0200

    map page done

--- lib/views/widgets/map_page.dart
+++ lib/views/widgets/map_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/services.dart';
+import 'package:task2_register_page/constant.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -8,7 +8,135 @@ class MapPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('hi mo ;)')),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: kPrimaryColor,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarColor: kPrimaryColor,
+        ),
+        title: const Text('Flutter Map'),
+        centerTitle: true,
+      ),
+      body: const AnimatedListView(),
     );
   }
 }
+
+class AnimatedListView extends StatefulWidget {
+  const AnimatedListView({
+    super.key,
+  });
+
+  @override
+  State<AnimatedListView> createState() => _AnimatedListViewState();
+}
+
+class _AnimatedListViewState extends State<AnimatedListView> {
+  final List<String> items = [];
+  final _key = GlobalKey<AnimatedListState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 10, bottom: 10),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(32),
+              color: kPrimaryColor,
+            ),
+            child: IconButton(
+              onPressed: insertItem,
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: AnimatedList(
+            key: _key,
+            initialItemCount: items.length,
+            itemBuilder: (context, index, animation) {
+              return SizeTransition(
+                sizeFactor: animation,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Card(
+                    color: kPrimaryColor,
+                    child: ListTile(
+                      leading: Text(
+                        items[index],
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 32,
+                        ),
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          deleteItem(index);
+                        },
+                        icon: const Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  void insertItem() {
+    var index = items.length;
+    items.add('item ${index + 1}');
+    _key.currentState!.insertItem(
+      index,
+      duration: const Duration(milliseconds: 500),
+    );
+  }
+
+  void deleteItem(int index) {
+    items.removeAt(index);
+
+    _key.currentState!.removeItem(
+      index,
+      (context, animation) {
+        return SizeTransition(
+          sizeFactor: animation,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Card(
+              color: Colors.red,
+              child: ListTile(
+                leading: Text(
+                  'deleted',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      duration: const Duration(milliseconds: 500),
+    );
+  }
+}
+
+
+
+
+// const Color(0xff0F6292)
+// const Color(0xffF0A04B)
